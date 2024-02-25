771fc61f160c2d55a07d2f13b611f6851a738da5
devmahin <mahinhowlader123@gmail.com>
2024-02-25 22:47:44 +0600

    new

--- script.js
+++ script.js
@@ -10,27 +10,68 @@
 // }
 
 
-function prototypeFun(name){
-    let person = Object.create(prototypeFun.prtotype )
-    person.name = name;
-    // console.log(person)
-    return person;
+
+//1111 ==========================================================================prototype vanila
+// function prototypeFun(name){
+//     let person = Object.create(prototypeFun.prtotype )
+//     person.name = name;
+//     // console.log(person)
+//     return person;
+// }
+
+// prototypeFun.prtotype = {
+//             eat(){
+//                 console.log(`"${this.name} is eating"`)
+//             },
+//             sleep(){
+//                 console.log(`${this.name} was sleeping`)
+//             }
+// }
+
+
+
+// let p1 = prototypeFun("mahin")
+// let p2 = prototypeFun("Adib")
+// let p3 = prototypeFun("hasan")
+// p2.eat()
+
+
+
+// 22222===================================== prototype convat new dia object create
+
+
+
+function prototypeFun (name){
+    this.myname = name;
 }
 
-prototypeFun.prtotype = {
-            eat(){
-                console.log(`"${this.name} is eating"`)
-            },
-            sleep(){
-                console.log(`${this.name} was sleeping`)
-            }
+
+prototypeFun.prototype ={
+    eat(){
+        console.log(`"person is eating"`)
+    },
+    sleep(){
+        console.log(`person was sleeping`)
+    }
 }
 
-
-
-let p1 = prototypeFun("mahin")
-let p2 = prototypeFun("Adib")
-let p3 = prototypeFun("hasan")
+let p1 = new prototypeFun("mahin")
+let p2 = new prototypeFun("Adib")
+let p3 = new prototypeFun("hasan")
 p2.eat()
 
 
+// class Persons{
+//     constructor(name){
+//      this.myname = name;
+//     }
+ 
+//      eat(){
+//          console.log(`"${name} is eating"`)
+//      }
+//      sleep(){
+//          console.log(`${name} was sleeping`)
+//      }
+//  }
+ 
+ 
